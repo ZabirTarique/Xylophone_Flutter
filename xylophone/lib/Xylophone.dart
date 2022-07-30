f7c6f99ca060920ee876ccd54e5787eb2d2b72be
KMZabirTarique <k.m.zabirtarique.baust@gmail.com>
2022-07-30 21:12:59 +0600

    Add the xylophone project

---
+++ xylophone/lib/Xylophone.dart
@@ -0,0 +1,56 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+
+class XyloPhone extends StatefulWidget {
+  const XyloPhone({Key? key}) : super(key: key);
+
+  @override
+  State<XyloPhone> createState() => _XyloPhoneState();
+}
+
+class _XyloPhoneState extends State<XyloPhone> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Xylophone'),
+        ),
+        backgroundColor: Colors.teal[100],
+        body: xyloPhoneUI(context),
+      ),
+    );
+  }
+
+  void playMusic(int musicNumber){
+    final player = AudioCache();
+    player.play('note$musicNumber.wav');
+  }
+
+  Widget userInterface({required int n,required Color color}){
+    return Expanded(
+        child: MaterialButton(
+          onPressed: () {
+            playMusic(n);
+          },
+          color: color, child: const Text(''),
+        ));
+  }
+
+  Widget xyloPhoneUI(BuildContext context) {
+    return Column(
+
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        userInterface(n: 1,color: Colors.yellowAccent),
+        userInterface(n: 2,color: Colors.greenAccent),
+        userInterface(n: 3,color: Colors.redAccent),
+        userInterface(n: 4,color: Colors.blueAccent),
+        userInterface(n: 5,color: Colors.amberAccent),
+        userInterface(n: 6,color: Colors.deepOrange),
+        userInterface(n: 7,color: Colors.pinkAccent),
+      ],
+    );
+  }
+}
